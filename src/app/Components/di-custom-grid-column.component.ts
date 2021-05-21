cd7e83a9e53d76ad72a0485adfaca70764557427
chanduravikanti <chanduravikanti@gmail.com>
2021-05-21 15:10:36 +0530

    Test2

--- src/app/Components/di-custom-grid-column.component.ts
+++ src/app/Components/di-custom-grid-column.component.ts
@@ -3,26 +3,17 @@ import { ColumnComponent } from '@progress/kendo-angular-grid';
 
 @Component({
   selector: 'di-custom-grid-column',
-  template: `<kendo-grid-column
-  #diCustomGridColumn
-  [field]="field"
-  [title]="title"
-  [width]="width">
-  <ng-template kendoGridFilterCellTemplate let-filter let-column="column">
-  <kendo-grid-string-filter-cell
-    [showOperators]="false"
-    [column]="column"
-    [filter]="filter">
-  </kendo-grid-string-filter-cell>
-</ng-template>
-</kendo-grid-column>`
+  templateUrl: './di-custom-grid-column.component.html'
 })
 export class DiCustomGridColumnComponent {
   @ViewChild(ColumnComponent) public realColumn: ColumnComponent | undefined;
   @Input() field: string = '';
   @Input() title: string = '';
   @Input() width: number = 0;
+  @Input() filter : any;
+  @Input() format : any;
 
+  
   constructor() { }
 
   ngAfterViewInit() {
